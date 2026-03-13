6a14f8141d9426f77506d70884bc724bc5484e03
facusteckler86 <facundomsteckler@gmail.com>
2026-03-12 23:50:27 -0300

    arreglo errores de tipeo y de llaves

--- MiApp.cs
+++ MiApp.cs
@@ -1,47 +1,40 @@
+Persona persona1 = new Alumno(); 
 
-Persona persona1 = new Persona();
 persona1.SetNombre("Facundo");
 persona1.SetApellido("Steckler");
 persona1.SetEdad(39);
 persona1.SetMail("facundomsteckler@gmail.com");
 
-Console.WriteLine(persona1.Saludar())
+// Faltaba el punto y coma al final
+Console.WriteLine(persona1.Saludar());
+
 
 public class Persona
 {
+    // Campos protegidos para que las clases hijas (como Alumno) puedan acceder a ellos
+    protected string? nombre;
+    protected string? apellido;
+    protected int edad;
+    protected string? mail;
 
-    private string? nombre;
-    private string? apellido;
-    private int edad;
-    private string? mail;
+    public void SetNombre(string nombre) => this.nombre = nombre;
+    public void SetApellido(string apellido) => this.apellido = apellido;
+    public void SetEdad(int edad) => this.edad = edad;
+    public void SetMail(string mail) => this.mail = mail;
 
-    public void SetNombre(string nombre)
+    // El método Saludar DEBE estar dentro de las llaves de la clase
+    public string Saludar()
     {
-        this.nombre = nombre;
+        return $"Hola, mi nombre es {this.nombre} {this.apellido}, tengo {this.edad} años y mi mail es {this.mail}";
     }
-
-    public void SetApellido(string apellido)
-    {
-        this.apellido = apellido;
-    }
-
-    public void SetEdad(int edad)
-    {
-        this.edad = edad;
-    }
-
-    public void SetMail(string mail)
-    {
-        this.mail = mail;
-
-    }
-
-  public string Saludar()
-{
-    
-    return $"Hola, mi nombre es {this.nombre} {this.apellido}, tengo {this.edad} años y mi mail es {this.mail}";
 }
 
+
+
+// El colocar los dos puntos, esta clase hereda los atributos de la clase Persona
+public class Alumno : Persona
+{
+    
 }
 
 
--- obj/Debug/net10.0/prueba NET.AssemblyInfo.cs
+++ obj/Debug/net10.0/prueba NET.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("prueba NET")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74a9415fe342b14614e8b69bc835c848912174d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dea456d9783c54672ee34ce69faefae65cc49439")]
 [assembly: System.Reflection.AssemblyProductAttribute("prueba NET")]
 [assembly: System.Reflection.AssemblyTitleAttribute("prueba NET")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
