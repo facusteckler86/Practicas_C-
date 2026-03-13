74a9415fe342b14614e8b69bc835c848912174d7
facusteckler86 <facundomsteckler@gmail.com>
2026-03-12 23:33:56 -0300

    armado de la clase persona

--- MiApp.cs
+++ MiApp.cs
@@ -1,1 +1,56 @@
-Console.WriteLine("mi primer linea en C#");
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Persona persona1 = new Persona();
+        persona1.SetNombre("Facundo");
+        persona1.SetApellido("Steckler");
+        persona1.SetEdad(39);
+        persona1.SetMail("facundomsteckler@gmail.com"); 
+    }
+}
+
+
+public class Persona
+{
+
+    private string nombre;
+    private string apellido;
+    private int edad;
+    private string mail;
+
+    public void SetNombre(string nombre)
+    {
+        this.nombre = nombre;
+    }
+
+    public void SetApellido(string apellido)
+    {
+        this.apellido = apellido;
+    }
+
+    public void SetEdad(int edad)
+    {
+        this.edad = edad;
+    }
+
+    public void SetMail(string mail)
+    {
+        this.mail = mail;
+
+    }
+
+    public static Saludar()
+    {
+        return $"Hola, mi nombre es {this.nombre}{this.apellido}, tengo {this.edad} y mi mail es {this.edad} ";
+    }
+
+}
+
+
+
+
+
+
+// Console.WriteLine("mi primer linea en C#");
+
